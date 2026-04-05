9e0ceaab702f433f6fab6696cdfe956945114eff
GOH00 <clockworld911@gmail.com>
2026-04-06 01:04:13 +0900

    fix workflow path

--- test_sample.py
+++ test_sample.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestSample(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(1 + 1, 3)
 
 if __name__ == "__main__":
     unittest.main()
